9a4e81c118462823e7d72d255baad7497d64b12e
jffryi <jefferyi.magtubo@gmail.com>
2024-03-27 10:21:47 -0500

    Changed back slashes to forward slashes in image paths

--- src/components/TN.tsx
+++ src/components/TN.tsx
@@ -25,11 +25,11 @@ export default function TN({movie, onClick} : {movie: {id: number, title: string
                 <p className="text-gray-400 text-xxs sm:text-sm">{movie.year.toString()}</p>
                 <div className="w-3.5 h-3.5 ml-1 flex items-center mr-1">
                     {movie.category === 'Movie' ? 
-                        <img src="public\assets\icon-nav-movies.svg" className="mt-3 sm:mt-2" />
+                        <img src="public/assets/icon-nav-movies.svg" className="mt-3 sm:mt-2" />
                         // <svg width="100%" height="100%" viewBox="0 0 25 25" className="mt-4"  xmlns="http://www.w3.org/2000/svg"><path  d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z" fill="#6b7280"/></svg> 
                     :
                     movie.category === 'TV Series' ?
-                        <img src="public\assets\icon-nav-tv-series.svg" className="mt-3 sm:mt-2" />
+                        <img src="public/assets/icon-nav-tv-series.svg" className="mt-3 sm:mt-2" />
                             // <svg width="100%" height="100%" viewBox="0 0 25 25" className="mt-4" xmlns="http://www.w3.org/2000/svg"><path d="M20 4.481H9.08l2.7-3.278L10.22 0 7 3.909 3.78.029 2.22 1.203l2.7 3.278H0V20h20V4.481Zm-8 13.58H2V6.42h10v11.64Zm5-3.88h-2v-1.94h2v1.94Zm0-3.88h-2V8.36h2v1.94Z" fill="#6b7280"/></svg> 
                     :
                     ""}             
